9600011f391b6d649fc4a283c1bc00f1577fb0c4
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-05-24 15:46:36 +0800

    更新 liangjihui.js

--- liangjihui.js
+++ liangjihui.js
@@ -9,22 +9,23 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/guapi\.liangjihui\.com\/front\/user\/memberInfo url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/liangjihui.js
+# 解锁会员信息
+^https?:\/\/guapi\.liangjihui\.com\/front\/(quote\/look.+|user\/memberInfo) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/liangjihui.js
+# 屏蔽顶部滚屏
+^https:\/\/ljh\.dianxiaoman\.com\/ljh\/api\/home\/getHomeList url reject
 
 [mitm]
-hostname = guapi.liangjihui.com
+hostname = guapi.liangjihui.com, ljh.dianxiaoman.com
 
 *************************************/
 
 
 var chxm1023 = JSON.parse($response.body);
 
-  chxm1023.data = {
-    "is_pay" : true,
-    "mobile" : "chxm1023",
-    "experience_card" : true,
-    "is_member" : true,
-    "end_time" : 4092599349
-  };
+  chxm1023.data.is_pay = true;
+  chxm1023.data.experience_card = true;
+  chxm1023.data.is_member = true;
+  chxm1023.data.end_time = 4092599349;
+  chxm1023.data.is_see = true;
 
 $done({body : JSON.stringify(chxm1023)});
