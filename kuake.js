aacca1f3415ea03eddab20be2eb3a1e9b92cca01
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-05-25 20:16:48 +0800

    更新 kuake.js

--- kuake.js
+++ kuake.js
@@ -9,7 +9,7 @@
 **************************************
 
 [rewrite_local]
-https?:\/\/drive.*\.quark\.cn\/.+\/clouddrive\/(member.+|distribute\/detail.+|capacity\/growth\/info) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/kuake.js
+^https?:\/\/drive.*\.quark\.cn\/.+\/clouddrive\/(member.+|distribute\/detail.+|capacity\/growth\/info) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/kuake.js
 
 [mitm]
 hostname = drive*.quark.cn
