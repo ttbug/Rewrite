9349eb9a98083aa098744dd74b5a51cf5e4b577d
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-05-25 17:39:00 +0800

    Update VideoShow.js

--- VideoShow.js
+++ VideoShow.js
@@ -1,5 +1,9 @@
 /*************************************
 
+项目名称：录屏
+下载地址：https://t.cn/A6ChM48g
+项目名称：大神P图
+下载地址：https://t.cn/A6p7eiRM
 项目名称：乐秀
 下载地址：https://t.cn/A6p7XAT4
 项目名称：多功能视频剪辑
@@ -11,69 +15,101 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/.*\.enjoy-mobi\.com\/zone\/.+\/(startPageAd\/getAds|iosPayClient\/payVerify) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/VideoShow.js
+^https?:\/\/.*\.(videoshowiosglobalserver|enjoy-mobi)\.com\/zone\/.+\/(iosPayClient\/(payVerify|imeiVerify)|startPageAd\/getAds) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/VideoShow.js
 
 [mitm]
-hostname = *.enjoy-mobi.com
+hostname = *.videoshowiosglobalserver.com, *.enjoy-mobi.com
 
 *************************************/
 
 
-var body = $response.body;
-var chxm1023 = JSON.parse(body);
+var chxm1023 = JSON.parse($response.body);
 const ad = '/startPageAd/getAds';
+const vipa = '/payVerify'
+const vipb = '/imeiVerify';
 const ua = $request.headers['User-Agent'] || $request.headers['user-agent'];
 
+//去除乐秀开屏
 if ($request.url.indexOf(ad) != -1){
   chxm1023 = {};
 }
 
+//录屏
+if (ua.indexOf('录屏') != -1) {
+  vip_id = "vrecorder_vip_yearly_3";
+  purchase = "2023-05-20 13:14:05 Etc/GMT";
+  expires = "2099-09-09 09:09:09 Etc/GMT";
+}
+
+//大神P图
+if (ua.indexOf('magicut_US') != -1) {
+  vip_id = "mg_vip_year";
+  purchase = "2023-05-20 13:14:05";
+  expires = "2099-09-09 09:09:09";
+}
+
+//乐秀
+if (ua.indexOf('VideoShow') != -1) {
+  vip_id = "vip_privilege_yearly_3";
+  purchase = "2023-05-20 13:14:05";
+  expires = "2099-09-09 09:09:09";
+}
+
+//多功能视频剪辑
 if (ua.indexOf('多功能视频剪辑') != -1) {
+  vip_id = "vip_yearly_3";
+  purchase = "2023-05-20 13:14:05";
+  expires = "2099-09-09 09:09:09";
+}
+
+//会员信息
+if ($request.url.indexOf(vipa) != -1){
   chxm1023 = {
-  "is_trial_period" : "true",
+  "expires_date" : (expires),
   "isBlacklist" : "0",
   "retCode" : "1",
   "status" : "0",
-  "transaction_id" : "666666666666666",
+  "transaction_id" : "310001314520000",
   "retMsg" : "success",
   "isRepeatBind" : "0",
   "isUsedPromocode" : "0",
   "subscrib_type" : "2",
   "current_date" : "2023-05-20 13:14:00",
   "isReportPrice" : "0",
-  "product_id" : "vip_yearly_3",
-  "purchase_date" : "2023-05-20 13:14:05",
-  "original_transaction_id" : "666666666666666",
+  "product_id" : (vip_id),
+  "purchase_date" : (purchase),
+  "original_transaction_id" : "310001314520000",
   "cancellation_date" : "",
   "promotional_offer_id" : "",
   "recallLabelType" : "0",
   "is_in_intro_offer_period" : "false",
   "msg" : "验证成功",
-  "expires_date" : "2099-09-09 09:09:09"
+  "is_trial_period" : "true"
 };}
 
-if (ua.indexOf('VideoShow') != -1) {
+if ($request.url.indexOf(vipb) != -1){
   chxm1023 = {
-  "is_trial_period" : "true",
-  "isBlacklist" : "0",
-  "retCode" : "1",
-  "status" : "0",
-  "transaction_id" : "666666666666666",
-  "retMsg" : "success",
-  "isRepeatBind" : "0",
-  "isUsedPromocode" : "0",
   "subscrib_type" : "2",
+  "retCode" : 1,
   "current_date" : "2023-05-20 13:14:00",
-  "isReportPrice" : "0",
-  "product_id" : "vip_privilege_yearly_3",
-  "purchase_date" : "2023-05-20 13:14:05",
-  "original_transaction_id" : "666666666666666",
-  "cancellation_date" : "",
-  "promotional_offer_id" : "",
-  "recallLabelType" : "0",
-  "is_in_intro_offer_period" : "false",
-  "msg" : "验证成功",
-  "expires_date" : "2099-09-09 09:09:09"
+  "isUpdateUuid" : 0,
+  "retMsg" : "success",
+  "isUsedPromocode" : 0,
+  "isDiscount" : 1,
+  "product_id" : (vip_id),
+  "renew_status" : 0,
+  "label_type" : 0,
+  "list" : [
+    {
+      "current_date" : "2023-05-20 13:14:00",
+      "original_transaction_id" : "310001314520000",
+      "order_status" : 1,
+      "product_id" : (vip_id),
+      "purchase_date" : (purchase),
+      "expires_date" : (expires)
+    }
+  ],
+  "recallLabelType" : 0
 };}
 
 $done({body : JSON.stringify(chxm1023)});
