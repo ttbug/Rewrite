77e7889339cd003392737cb0481170b94e7b19f5
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-05-29 19:19:18 +0800

    更新 duitang.js

--- duitang.js
+++ duitang.js
@@ -9,25 +9,24 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/api\.duitang\.com\/napi url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/duitang.js
+^https:\/\/.*\.duitang\.com\/napi url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/duitang.js
 
 [mitm] 
-hostname = api.duitang.com
+hostname = *.duitang.com
 
 *************************************/
 
 
 var body = $response.body;
 
+body = body.replace(/\"vip":\w+/g, '\"vip":true');
+body = body.replace(/\"svip_mechanism":\d+/g, '\"svip_mechanism":1');
 body = body.replace(/\"is_life_artist":\w+/g, '\"is_life_artist":true');
 body = body.replace(/\"isnew":\w+/g, '\"isnew":true');
-body = body.replace(/\"is_root":\d+/g, '\"is_root":1');
-body = body.replace(/\"svip_mechanism":\d+/g, '\"svip_mechanism":1');
-body = body.replace(/\"vip":\w+/g, '\"vip":true');
-body = body.replace(/\"vipType":"\w+"/g, '\"vipType":"SVIP"');
+body = body.replace(/\"vip_end_at":\d+/g, '\"vip_end_at":4092599350');
+body = body.replace(/\"vip_end_at_mills":\d+/g, '\"vip_end_at_mills":4092599350000');
 body = body.replace(/\"vip_level":\d+/g, '\"vip_level":11');
 body = body.replace(/\"latest_vip_level":\d+/g, '\"latest_vip_level":11');
-body = body.replace(/\"vip_end_at_mills":\d+/g, '\"vip_end_at_mills":4092599350000');
 body = body.replace(/\"is_certify_user":\w+/g, '\"is_certify_user":true');
 body = body.replace(/\"be_follow_count":\d+/g, '\"be_follow_count":1000000');
 body = body.replace(/\"follow_count":\d+/g, '\"follow_count":1000000');
