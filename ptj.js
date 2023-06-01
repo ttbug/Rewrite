b10cc4c049482374e6e7f9e8040a60114394784f
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-06-01 19:32:50 +0800

    更新 ptj.js

--- ptj.js
+++ ptj.js
@@ -1,17 +1,17 @@
 /****************************************
 
-项目名称：拼图酱 解锁VIP
+项目名称：拼图酱
 下载地址：https://t.cn/AiuTkkXO
-使用声明：仅供学习与交流，请勿转载与贩卖！⚠️
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
 
 *****************************************
 
 [rewrite_local]
-
 https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/ptj.js
 
 [mitm]
-
 hostname = buy.itunes.apple.com
 
 ****************************************/
