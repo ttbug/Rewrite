6014e9d17325e44b43d8d4bb3219d52358d6ff92
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-06-04 10:22:07 +0800

    更新 dsj.js

--- dsj.js
+++ dsj.js
@@ -2,20 +2,31 @@
 
 项目名称：电视家
 下载地址：https://t.cn/A6KxaEw8
-使用声明：仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
 
 **************************************
 
-http:\/\/123\.56\.125\.184\/api\/v3\/user\/info url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/dsj.js
+[rewrite_local]
+^http[s]?:\/\/(share\.dianshihome\.com\/api\/user\/base\/info|123\.56\.125\.184\/api\/.+\/user\/info|api\.gaoqingdianshi\.com\/api\/ad\/mobile\/config) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/dsj.js
+
+[mitm]
+hostname = share.dianshihome.com, api.gaoqingdianshi.com
 
 *************************************/
 
 
-var body = $response.body;
-var chxm1023 = JSON.parse(body);
+var chxm1023 = JSON.parse($response.body);
 
+chxm1023.data.sex = 1;
+chxm1023.data.activeProduct = 2;
+chxm1023.data.isTopLevel = true;
 chxm1023.data.equityTime = 4092599349000;
+chxm1023.data.pActiveTime = 4092599349000;
 chxm1023.data.adEquityTime = 4092599349000;
-chxm1023.data.exclusiveTime = 4092599349000;
+chxm1023.data.mine = [];
+chxm1023.data.homePopWindow = [];
+chxm1023.data.homeVipOperate= [];
 
 $done({body : JSON.stringify(chxm1023)});
