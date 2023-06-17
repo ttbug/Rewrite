003dbdd0fd14fcda5b3f8a8ff20dd7dd56f2de59
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-06-18 00:48:30 +0800

    更新 picsart.js

--- picsart.js
+++ picsart.js
@@ -1,6 +1,6 @@
 /*************************************
 
-项目功能：Picsart美易 解锁VIP
+项目功能：Picsart美易
 下载地址：https://t.cn/A6MxZ5q1
 脚本作者：chxm1023
 电报频道：https://t.me/chxm1023
@@ -9,7 +9,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/api\.meiease\.cn\/shop\/subscription\/validate url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/picsart.js
+^https?:\/\/api\.meiease\.cn\/shop\/subscription\/(validate|apple\/purchases) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/picsart.js
 
 [mitm]
 hostname = api.meiease.cn
@@ -17,6 +17,43 @@ hostname = api.meiease.cn
 *************************************/
 
 
-var chxm1023={"warning":"仅供学习，禁止转载或售卖","tgchannel":"https://t.me/chxm1023","feedback":"chxm1023","status":"success","reason":"ok","response":{"purchase_date":1645263154000,"expire_date":4092599350000,"app":"com.picsart.editor","subscription_id":"com.picsart.editor.subscription_yearly","order_id":"300001048350229","original_order_id":"600001048350229","status":"SUBSCRIPTION_PURCHASED","is_trial":true,"winback_screen_id":1,"is_eligible_for_introductory":false,"plan_meta":{},"limitation":{"max_count":1000,"limits_exceeded":false},"is_eligible_for_grant":true}};
+var chxm1023 = JSON.parse($response.body);
+
+chxm1023 = {
+  "status" : "success",
+  "response" : [
+    {
+      "status" : "SUBSCRIPTION_PURCHASED",
+      "order_id" : "490001314520000",
+      "original_order_id" : "490001314520000",
+      "is_trial" : true,
+      "plan_meta" : {
+        "storage_limit_in_mb" : 20480,
+        "frequency" : "yearly",
+        "scope_id" : "full",
+        "id" : "com.picsart.editor.subscription_yearly",
+        "product_id" : "subscription_yearly",
+        "level" : 2000,
+        "auto_renew_product_id" : "com.picsart.editor.subscription_yearly",
+        "type" : "renewable",
+        "tier_id" : "gold_old",
+        "permissions" : [
+          "premium_tools_standard",
+          "premium_tools_ai"
+        ],
+        "description" : "china"
+      },
+      "limitation" : {
+        "max_count" : 5,
+        "limits_exceeded" : false
+      },
+      "reason" : "ok",
+      "subscription_id" : "com.picsart.editor.subscription_yearly",
+      "is_eligible_for_introductory" : false,
+      "purchase_date" : 1687020148000,
+      "expire_date" : 4092599349000
+    }
+  ]
+};
 
 $done({body: JSON.stringify(chxm1023)});
