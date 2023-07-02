eaa07198bc80bf59486dd1c03e73c474b699760c
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-07-02 23:27:19 +0800

    Create duitang2.js

---
+++ duitang2.js
@@ -0,0 +1,19 @@
+/*************************************
+
+项目功能：推糖-壁纸头像美图社区
+下载地址：https://t.cn/Ai3pMcdl
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https:\/\/.*\.duitang\.com\/napi\/people\/me url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/duitang2.js
+
+[mitm] 
+hostname = *.duitang.com
+
+*************************************/
+
+
