121824ba62da1538b7b6b5d046fcbc9e52a9ea46
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-07-04 19:12:51 +0800

    更新 Cb.js

--- Cb.js
+++ Cb.js
@@ -9,7 +9,7 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/cubox\.(pro|app)\/c\/api\/userInfo url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Cb.js
+^https:\/\/cubox\.(pro|cc)\/c\/api\/userInfo url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Cb.js
 
 [mitm]
 hostname = cubox.*
