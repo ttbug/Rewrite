ce676d3090832ca984784d1506e56981a150d120
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-07-04 09:31:13 +0800

    更新 duitang.js

--- duitang.js
+++ duitang.js
@@ -9,7 +9,7 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/.*\.duitang\.com\/napi url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/duitang.js
+^http[s]?:\/\/.*\.duitang\.com\/napi url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/duitang.js
 
 [mitm] 
 hostname = *.duitang.com
