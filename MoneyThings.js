b00c239c458020702db20134731835effd876470
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-07-06 13:33:13 +0800

    更新 MoneyThings.js

--- MoneyThings.js
+++ MoneyThings.js
@@ -1,23 +1,25 @@
-########################################
+/*************************************
 
-#项目名称：MoneyThings-记账
-#下载地址：https://t.cn/A6pg9nPW
-#项目名称：SalesCat-RevenueCat客户端
-#下载地址：https://t.cn/A6pFPFKM
-#项目名称：MatrixClocca-矩阵时钟
-#下载地址：https://t.cn/A6pFhtw1
-#脚本作者：chxm1023
-#电报频道：https://t.me/chxm1023
-#使用声明：⚠️仅供参考，🈲转载与售卖！
+项目名称：MoneyThings-记账
+下载地址：https://t.cn/A6pg9nPW
+项目名称：SalesCat-RevenueCat客户端
+下载地址：https://t.cn/A6pFPFKM
+项目名称：MatrixClocca-矩阵时钟
+下载地址：https://t.cn/A6pFhtw1
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
 
-########################################
+**************************************
 
+[rewrite_local]
 ^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/MoneyThings.js
 ^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/MoneyThings.js
 
+[mitm]
 hostname = api.revenuecat.com
 
-########################################
+*************************************/
 
 
 const chxm1023 = {};
