a06f8a5c50a9603bf5795231c994d4408a7b34ac
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-07-06 15:34:12 +0800

    更新 chatai.js

--- chatai.js
+++ chatai.js
@@ -9,59 +9,43 @@
 *****************************************
 
 [rewrite_local]
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/chatai.js
+^https^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/chatai.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/chatai.js
 
 [mitm]
 hostname = api.revenuecat.com
 
-****************************************/
+*************************************/
 
 
-var chxm1023 = JSON.parse($response.body);
-
-chxm1023 = {
-  "request_date_ms" : 1688103683707,
-  "request_date" : "2023-06-30T05:41:23Z",
-  "subscriber" : {
-    "non_subscriptions" : {
-
-    },
-    "first_seen" : "2023-06-30T05:41:19Z",
-    "original_application_version" : null,
-    "entitlement" : {
-
-    },
-    "other_purchases" : {
-
-    },
-    "management_url" : null,
-    "subscriptions" : {
-      "com.palligroup.gpt3.yearlyyy" : {
-        "Author" : "chxm1023",
-        "ownership_type" : "PURCHASED",
-        "expires_date" : "2099-09-09T09:09:09Z",
-        "warning" : "仅供学习，禁止转载或售卖",
-        "original_purchase_date" : "2022-09-09T09:09:09Z",
-        "Telegram" : "https://t.me/chxm1023",
-        "purchase_date" : "2022-09-09T09:09:09Z"
-      }
-    },
-    "entitlements" : {
-      "Advanced" : {
-        "Author" : "chxm1023",
-        "ownership_type" : "PURCHASED",
-        "product_identifier" : "com.palligroup.gpt3.yearlyyy",
-        "expires_date" : "2099-09-09T09:09:09Z",
-        "warning" : "仅供学习，禁止转载或售卖",
-        "original_purchase_date" : "2022-09-09T09:09:09Z",
-        "Telegram" : "https://t.me/chxm1023",
-        "purchase_date" : "2022-09-09T09:09:09Z"
-      }
-    },
-    "original_purchase_date" : null,
-    "original_app_user_id" : "$RCAnonymousID:2aa6b94e60984cd7ad40a8c995f54646",
-    "last_seen" : "2023-06-30T05:41:19Z"
-  }
+const chxm1023 = {};
+const chxm1024 = JSON.parse(typeof $response != "undefined" && $response.body || null);const app = 'cm';const list = {'cm':{name: 'Advanced', id: 'com.palligroup.gpt3.yearlyyy'}};
+const data = {
+	"Author": "chxm1023",
+	"Telegram" : "https://t.me/chxm1023",
+	"warning": "仅供学习，禁止转载或售卖",
+	"original_purchase_date": "2022-09-09T09:09:09Z",
+	"purchase_date": "2022-09-09T09:09:09Z",
+	"expires_date": "2099-09-09T09:09:09Z",
+	"ownership_type": "PURCHASED"
 };
 
-$done({body : JSON.stringify(chxm1023)});
+if (typeof $response == "undefined") {
+	delete $request.headers["x-revenuecat-etag"];
+	delete $request.headers["X-RevenueCat-ETag"];
+	chxm1023.headers = $request.headers;
+} else if (chxm1024 && chxm1024.subscriber) {
+	chxm1024.subscriber.subscriptions = chxm1024.subscriber.subscriptions || {};
+	chxm1024.subscriber.entitlement = chxm1024.subscriber.entitlement || {};
+     for (const i in list) {
+if (new RegExp(`^${i}`, `i`).test(app)) {
+	chxm1024.subscriber.subscriptions[list[i].id] = data;
+	chxm1024.subscriber.entitlements[list[i].name] = JSON.parse(JSON.stringify(data));
+	chxm1024.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
+			break;
+		}
+	}
+   chxm1023.body = JSON.stringify(chxm1024);
+}
+
+$done(chxm1023);
