b2f5e38ab012eb94563109658a7d0ac970e1cc04
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-07-08 22:50:22 +0800

    更新 yiyan.js

--- yiyan.js
+++ yiyan.js
@@ -1,6 +1,6 @@
 /*************************************
 
-项目功能：一言 解锁终身会员
+项目功能：一言 解锁会员/净化广告
 下载地址：https://t.cn/Aini0veg
 脚本作者：chxm1023
 电报频道：https://t.me/chxm1023
@@ -9,11 +9,12 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/app\.yiyan\.art\/yiyan\/ url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/Rewrite/yiyan.js
+^https:\/\/app\.yiyan\.art\/yiyan url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/Rewrite/yiyan.js
 
 [mitm]
 hostname = app.yiyan.art
 
 *************************************/
 
-var chxm1023 = $response.body.replace(/viptype":"\d+"/g, 'viptype":"4"');$done(chxm1023);
+
+var chxm1023 = $response.body.replace(/viptype":"\d+"/g, 'viptype":"4"').replace(/banner":\[.+\]/g, 'banner":[]');$done(chxm1023);
