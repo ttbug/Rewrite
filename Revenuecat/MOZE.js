68d727aae46b1159c45cbf556a965ec5139c07c2
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-07-13 15:47:11 +0800

    Update and rename MOZE.js to Revenuecat/MOZE.js

--- Revenuecat/MOZE.js
+++ Revenuecat/MOZE.js
@@ -9,8 +9,8 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/MOZE.js
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/MOZE.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/MOZE.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/MOZE.js
 
 [mitm]
 hostname = api.revenuecat.com
