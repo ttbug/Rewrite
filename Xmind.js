35018f1ad6dd81d9b78ab5f0b06107fb27209249
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-07-14 13:46:52 +0800

    更新 Xmind.js

--- Xmind.js
+++ Xmind.js
@@ -19,10 +19,14 @@ hostname = www.xmind.*
 
 
 var chxm1023 = JSON.parse($response.body);
-  chxm1023.raw_data = "bKnTeMMu/TJ86ZFoUqG+sfKC1nitD8b/NaUT93UBc0woo0BF5uBVYXsHUQJkQxLHGlZCbdnKaKzZ4iljM9xddkyMycpUbAtDoywnr4RNzxqXY20pDM0Bdgm+HssaS1RworH8aaaf0IYnnGplxIuqMyT9107PrenF0MBUIaKU9Cw=";
-  chxm1023.license = {
-    "status": "sub",
-    "expireTime": 4092599349000
-  };
+
+chxm1023 = {
+  "license" : {
+    "status" : "sub",
+    "expireTime" : 4092599349000
+  },
+  "_code" : 200,
+  "raw_data" : "bKnTeMMu/TJ86ZFoUqG+sfKC1nitD8b/NaUT93UBc0woo0BF5uBVYXsHUQJkQxLHGlZCbdnKaKzZ4iljM9xddkyMycpUbAtDoywnr4RNzxqXY20pDM0Bdgm+HssaS1RworH8aaaf0IYnnGplxIuqMyT9107PrenF0MBUIaKU9Cw="
+};
 
 $done({body : JSON.stringify(chxm1023)});
