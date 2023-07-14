452337789b8c3df73bf2bb75eeb25f2eae7aabcc
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-07-14 15:56:22 +0800

    Update yiyue.js

--- Revenuecat/yiyue.js
+++ Revenuecat/yiyue.js
@@ -9,8 +9,8 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/yiyue.js
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/yiyue.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/yiyue.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/yiyue.js
 
 [mitm]
 hostname = api.revenuecat.com
@@ -37,9 +37,9 @@ if (typeof $response == "undefined") {
    //"expires_date": "2099-09-09T09:09:09Z",
    "ownership_type": "PURCHASED"};
 for (const i in list) { if (new RegExp(`^${i}`, `i`).test(app)) {
-	chxm1024.subscriber.subscriptions[list[i].id] = data;
-	chxm1024.subscriber.entitlements[list[i].name] = JSON.parse(JSON.stringify(data));
-	chxm1024.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
+  chxm1024.subscriber.subscriptions[list[i].id] = data;
+  chxm1024.subscriber.entitlements[list[i].name] = JSON.parse(JSON.stringify(data));
+  chxm1024.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
                 break;
           }
     }
