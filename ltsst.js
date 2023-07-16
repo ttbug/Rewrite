82c463ba57acceddc5d529aca4f2581ab6987f72
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-07-17 00:48:34 +0800

    更新 ltsst.js

--- ltsst.js
+++ ltsst.js
@@ -1,6 +1,6 @@
 /*************************************
 
-项目名称：旅途随身听
+项目名称：旅途随身听(APP+小程序)
 下载地址：https://t.cn/A60UTceU
 脚本作者：chxm1023
 电报频道：https://t.me/chxm1023
@@ -9,7 +9,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/www\.1314zhilv\.com\/ltsstnew\/(user\/getInfo|guideScenic\/getScenicAllMarkerList) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/ltsst.js
+^https?:\/\/www\.1314zhilv\.com\/ltsstnew\/(user.*\/getInfo|guideScenic) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/ltsst.js
 
 [mitm]
 hostname = www.1314zhilv.com
@@ -23,8 +23,6 @@ body = body.replace(/\"identityType":\d+/g, '\"identityType":2');
 body = body.replace(/\"levDesc":".*?"/g,'\"levDesc":"永久会员"');
 body = body.replace(/\"isActivate":\d+/g,'\"isActivate":1');
 body = body.replace(/\"audioType":\d+/g, '\"audioType":1');
-body = body.replace(/\"isShowMap":\d+/g, '\"isShowMap":1');
-body = body.replace(/\"isMustVisit":\d+/g, '\"isMustVisit":2');
 body = body.replace(/\"isAudition":\d+/g, '\"isAudition":2');
 
 $done({body});
