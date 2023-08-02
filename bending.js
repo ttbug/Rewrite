694cdbbb212653587f7e9188aef626f3f791e686
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-08-02 15:30:30 +0800

    更新 bending.js

--- bending.js
+++ bending.js
@@ -10,7 +10,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/.*\.oracle\.bendingspoonsapps\.com\/v\d\/(users\/setup|purchases\/verify) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/bending.js
+^https?:\/\/.*\.oracle\.bendingspoonsapps\.com\/v\d\/(users\/.+|purchases\/verify) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/bending.js
 
 [mitm]
 hostname = *.oracle.bendingspoonsapps.com
