ef55cde7c2b2d5278bdcf20083b9ac65e0e4e8db
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-08-05 10:09:47 +0800

    更新 tripsy.js

--- tripsy.js
+++ tripsy.js
@@ -17,16 +17,4 @@ hostname = firstclass.tripsy.app
 *************************************/
 
 
-var chxm1023 = JSON.parse($response.body);
-
-chxm1023 = {
-  "is_premium" : true,
-  "expiration_intent" : 0,
-  "is_trial" : true,
-  "subscription" : "premium.yearly.29.99.v4",
-  "expiration_date" : "2099-09-09T09:09:09Z",
-  "is_introductory_offer" : false,
-  "is_in_billing_retry_period" : false
-};
-
-$done({body : JSON.stringify(chxm1023)});
+if ($response.body != 'undefined'){var chxm1023 = JSON.parse($response.body);chxm1023 = {"is_premium":true,"expiration_intent":0,"is_trial":true,"subscription":"premium.yearly.29.99.v4","expiration_date":"2099-09-09T09:09:09Z","is_introductory_offer":false,"is_in_billing_retry_period":false};$done({body:JSON.stringify(chxm1023)})};$done();
